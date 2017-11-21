39efb645b6a692c6e0829a80727e0dcbd5573647
刘艳 <liuyan@hiwie.com>
2017-11-21 20:00:15 +0800

    Initial commit

---
+++ src/css.js
@@ -0,0 +1,10 @@
+require('./sass/index.scss');
+require('./sass/store.scss');
+require('./sass/detail.scss');
+require('./sass/comment.scss');
+require('./sass/shoppingCar.scss');
+require('./sass/orderConfirm.scss');
+require('./sass/address.scss');
+require('./sass/myOrder.scss');
+require('./sass/personal.scss');
+require('./sass/coupons.scss');
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+    // 配置入口
+    entry: {
+        '/css/index':__dirname +'/src/css.js'
+    },
+    // 编译后的文件路径
+    output: {
+
+        path: __dirname +'/src/html', // 文件路径
+        filename: '[name].js' // 文件名称
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: extractSass.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+        }]
+    },
+    // 辅助的插件
+    plugins:[
+        new BrowserSyncPlugin({
+            host:'localhost', // 实时监听，webpack -w 可以实时更新硬盘中的文件js，css
+            port:8080,
+            file:'',
+            server:{
+                baseDir:'./src/html'
+            }
+        }),
+        extractSass
+    ]
+}
