280513c7770a2b5a6a6a00fae59b9a9a5580957d
刘艳 <liuyan@hiwie.com>
2017-11-23 23:05:30 +0800

    精品商城

--- src/javascript/util.js
+++ src/javascript/util.js
@@ -4,3 +4,14 @@ function get_query_string(name) {
     if(values!==null)return  unescape(values[2]);
     return null;
 }
+
+
+function getJsonFromUrl(url) {
+    var qs = url.split("?")[1];
+    var result = {};
+    qs.split("&").forEach(function(part) {
+        var item = part.split("=");
+        result[item[0]] = decodeURIComponent(item[1]);
+    });
+    return result;
+}
